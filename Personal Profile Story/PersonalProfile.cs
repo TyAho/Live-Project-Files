48bede806877ec284bcf87acaa867bc49a54b55d
TyAho <49496998+TyAho@users.noreply.github.com>
2019-09-21 20:45:47 -0700

    Add files via upload

---
+++ Personal Profile Story/PersonalProfile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ManagementPortal.Models
+{
+    public class PersonalProfile
+    {
+
+        [Key]
+        [ForeignKey("Person")]
+        public string ProfileID { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "About Me must be between 1 and 100 characters")]
+        [Display(Name = "AboutMe")]
+        public string AboutMe { get; set; }
+
+
+        [Required]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Tagline must be between 1 and 100 characters")]
+        [Display(Name = "Tagline")]
+        public string TagLine { get; set; }
+
+        public virtual ApplicationUser Person { get; set; }
+
+        public static implicit operator PersonalProfile(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
