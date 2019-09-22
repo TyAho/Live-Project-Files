17b6ee0ad92da4a8a0a0cd1fcd94e14c736b0d35
TyAho <49496998+TyAho@users.noreply.github.com>
2019-09-21 19:24:34 -0700

    Add files via upload

---
+++ IdentityModels.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using ManagementPortal.Enums;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ManagementPortal.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+        // PROFILE PICTURE
+        public byte[] ProfilePicture { get; set; }
+
+        //Set DisplayName with first name and last initial
+        public string DisplayName { get { return FirstName + " " + LastName.Substring(0, 1); } internal set { FirstName = value; LastName = value; } }
+        [Required(ErrorMessage = "Required Field. Please enter a First Name:"), Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Required Field. Please enter a Last Name: "), Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Required Field. Please enter a Work Type >> Leadman,Foreman,ExpMBA, or NewMBA: "), Display(Name = "Work Type")]
+        public WorkType WorkType { get; set; }
+        public string UserRole { get; set; }
+        [Display(Name = "Suspended")]
+        public bool Suspended { get; set; }
+
+        public string FullName { get { return (FirstName + " " + LastName); } }
+        public virtual List<Job> Jobs { get; set; }
+        public virtual List<Schedule> Schedules { get; set; }
+        public virtual PersonalProfile Profile { get; set; }
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+        //add the user identity here for the aspnetuser table.
+
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        // Begin DbSets
+
+        public DbSet<CalendarEvent> CalendarEvents { get; set; }
+        public DbSet<CreateUserRequest> CreateUserRequests { get; set; }
+
+        public DbSet<ChatMessage> ChatMessages { get; set; }
+
+        public DbSet<CompanyNews> CompanyNews { get; set; }
+
+        public DbSet<Job> Jobs { get; set; }
+        public DbSet<JobSite> JobSites { get; set; }
+        public DbSet<ShiftTime> ShiftTime { get; set; }
+        public DbSet<JobOther> JobOthers { get; set; }
+        public DbSet<PersonalProfile> Profile { get; set; }
+        public DbSet<Schedule> Schedules { get; set; }
+        public DbSet<JobAction> JobActions { get; set; }
+
+        //public virtual DbSet<CalEvent> CalendarEvents { get; set; }
+
+        // End DbSets
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //FluentAPI Model Config Goes Here
+        }
+
+        //Scafolding databases
+        //public System.Data.Entity.DbSet<ManagementPortal.Models.Jobsite> Jobsites { get; set; }
+        //Area specific scaffolding databases
+        public System.Data.Entity.DbSet<ManagementPortal.Areas.Employee.Models.EmployeeInfo> Employees { get; set; }
+    }
+}
---
+++ PersonalProfilesController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ManagementPortal.Models;
+
+namespace ManagementPortal.Controllers
+{
+    public class PersonalProfilesController : ApplicationBaseController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: PersonalProfiles
+        public ActionResult Index()
+        {
+            var personalProfiles = db.Profile.Include(p => p.Person);
+            return View(personalProfiles.ToList());
+        }
+
+        // GET: PersonalProfiles/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PersonalProfile PersonalProfiles = db.Profile.Find(id);
+            if (PersonalProfiles == null)
+            {
+                return HttpNotFound();
+            }
+            return View(PersonalProfiles);
+        }
+
+        // GET: PersonalProfiles/Create
+        public ActionResult Create()
+        {
+            ViewBag.ProfileID = new SelectList(db.Users, "Id", "DisplayName");
+            return View();
+        }
+
+        // POST: PersonalProfiles/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProfileID,AboutMe,TagLine")] PersonalProfile PersonalProfiles)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Profile.Add(PersonalProfiles);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProfileID = new SelectList(db.Users, "Id", "DisplayName", PersonalProfiles.ProfileID);
+            return View(PersonalProfiles);
+        }
+
+        // GET: PersonalProfiles/Edit/5
+        public ActionResult Edit(string id, string ProfileID)
+        {
+            //var ProfileID = Request.QueryString["id"];
+            //return View();
+
+
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PersonalProfile PersonalProfiles = db.Profile.Find(id);
+            if (PersonalProfiles == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProfileID = new SelectList(db.Users, "ProfileID", "DisplayName", PersonalProfiles.ProfileID);
+            return View(PersonalProfiles);
+        }
+
+        // POST: PersonalProfiles/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProfileID,AboutMe,TagLine")] PersonalProfile PersonalProfiles, IndexViewModel ProfileID)
+        {
+            //var ProfileID = Request["ProfileID"];
+            //var AboutMe = Request["About Me"];
+            //var TagLine = Request["TagLine"];
+
+            //return RedirectToAction(ProfileID);
+
+
+            if (ModelState.IsValid)
+            {
+                db.Entry(PersonalProfiles).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProfileID = new SelectList(db.Users, "ProfileID", "DisplayName", PersonalProfiles.ProfileID);
+            return View(PersonalProfiles);
+        }
+
+        // GET: PersonalProfiles/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PersonalProfile PersonalProfiles = db.Profile.Find(id);
+            if (PersonalProfiles == null)
+            {
+                return HttpNotFound();
+            }
+            return View(PersonalProfiles);
+        }
+
+        // POST: PersonalProfiles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            PersonalProfile PersonalProfiles = db.Profile.Find(id);
+            db.Profile.Remove(PersonalProfiles);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
